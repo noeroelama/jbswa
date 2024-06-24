cb4de79745a89e7d56492e3cf2dd9295255f3ddc
Arif Nurwahid <arif@aqlis.sch.id>
2024-06-25 04:55:44 +0900

    update bat

--- automate/create_task.bat
+++ automate/create_task.bat
@@ -3,7 +3,8 @@
 set taskname="Wanesia JIBAS"
 set taskrun="C:\YIM\JIBAS\xampp\htdocs\jibas\jbswa\automate\send.bat"
 
-schtasks /create /tn %taskname% /tr %taskrun% /sc minute /mo 1 /f
+:: Create the task to run every minute with highest privileges and system account
+schtasks /create /tn %taskname% /tr %taskrun% /sc minute /mo 1 /ru "SYSTEM" /RL HIGHEST /F
 
 echo Task "%taskname%" berhasil dibuat untuk dijalankan tiap menit.
 pause
